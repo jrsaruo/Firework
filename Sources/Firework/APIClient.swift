8bcb06dbc90e3160cbee6fd90bd78110bb418af6
Yusaku Nishi <jrsaruo@gmail.com>
2021-04-19 15:46:22 +0900

    add: APIClient.send(_:, decodingCompletion:)

--- Sources/Firework/APIClient.swift
+++ Sources/Firework/APIClient.swift
@@ -6,10 +6,16 @@
 //
 
 import Alamofire
+import Foundation
 
 public struct APIClient {
     
     private init() {}
+    
+    /// A default decoder used to decode JSON in the `APIClient.send(_:, decodingCompletion:)` method.
+    ///
+    /// You can use this property when you want to use a common decoder in your app.
+    public static let defaultJSONDecoder = JSONDecoder()
     
     public static func send<Request: APIRequest>(_ request: Request,
                                                  completion: @escaping (Result<Void, Error>) -> Void) {
@@ -23,6 +29,26 @@ public struct APIClient {
             case .failure(let error):
                 result = .failure(error)
             }
+        }
+    }
+    public static func send<Request: DecodingRequest>(_ request: Request,
+                                                      decodingCompletion: @escaping (Result<Request.Response, Error>) -> Void) {
+        request.alamofireRequest
+            .responseData { response in
+                let result: Result<Request.Response, Error>
+                defer { decodingCompletion(result) }
+                
+                switch response.result {
+                case .success(let data):
+                    do {
+                        let decoded = try defaultJSONDecoder.decode(Request.Response.self, from: data)
+                        result = .success(decoded)
+                    } catch {
+                        result = .failure(error)
+                    }
+                case .failure(let error):
+                    result = .failure(error)
+                }
         }
     }
 }
