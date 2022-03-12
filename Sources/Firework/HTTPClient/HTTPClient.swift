ccbf854ae8530007c4421625ab406bdfbe093b8c
Yusaku Nishi <jrsaruo@gmail.com>
2022-03-12 22:34:34 +0900

    add: HTTPClient

---
+++ Sources/Firework/HTTPClient/HTTPClient.swift
@@ -0,0 +1,68 @@
+//
+//  HTTPClient.swift
+//  
+//
+//  Created by Yusaku Nishi on 2022/03/12.
+//
+
+import Foundation
+
+// MARK: - HTTPClientConfiguration -
+
+public final class HTTPClientConfiguration {
+    
+    public static let shared = HTTPClientConfiguration()
+    
+    /// A default decoder used to decode JSON in the `HTTPClient.send(_:, decodingCompletion:)` method.
+    ///
+    /// You can use this property when you want to use a common decoder in your app.
+    /// If you want to use a different decoder for each request,
+    /// implement the `preferredJSONDecoder` property in the request type that conforms to the ``DecodingRequest`` protocol.
+    public lazy var defaultJSONDecoder = JSONDecoder()
+}
+
+// MARK: - HTTPClient -
+
+public struct HTTPClient<Adaptor: HTTPClientAdaptor> {
+    
+    public var configuration = HTTPClientConfiguration.shared
+    let adaptor: Adaptor
+    
+    /// Send a request and receive the simple response.
+    /// - Parameters:
+    ///   - request: An instance of the request type that conforms to the ``APIRequest`` protocol.
+    ///   - queue: The queue on which the completion handler is called. The default is `.main`.
+    ///   - completion: The handler to be executed once the request has finished.
+    public func send<Request: APIRequest>(_ request: Request,
+                                          receiveOn queue: DispatchQueue = .main,
+                                          completion: @escaping (Result<Data?, Adaptor.Failure>) -> Void) {
+        adaptor.send(request, receiveOn: queue, completion: completion)
+    }
+    
+    /// Send a request and decode the response JSON.
+    /// - Parameters:
+    ///   - request: An instance of the request type that conforms to the ``DecodingRequest`` protocol.
+    ///   - queue: The queue on which the completion handler is called. The default is `.main`.   
+    ///   - decodingCompletion: The handler to be executed once the request and decoding has finished.
+    public func send<Request: DecodingRequest>(_ request: Request,
+                                               receiveOn queue: DispatchQueue = .main,
+                                               decodingCompletion: @escaping (Result<Request.Response, Error>) -> Void) {
+        adaptor.send(request, receiveOn: queue, completion: { (result: Result<Data, Adaptor.Failure>) in
+            let finalResult: Result<Request.Response, Error>
+            defer { decodingCompletion(finalResult) }
+            
+            switch result {
+            case .success(let data):
+                do {
+                    let decoder = Request.preferredJSONDecoder ?? configuration.defaultJSONDecoder
+                    let decoded = try decoder.decode(Request.Response.self, from: data)
+                    finalResult = .success(decoded)
+                } catch {
+                    finalResult = .failure(error)
+                }
+            case .failure(let error):
+                finalResult = .failure(error)
+            }
+        })
+    }
+}
