4fa089b44f90e6960554c0939581999b8ac8471f
Yusaku Nishi <jrsaruo@gmail.com>
2022-03-19 15:02:34 +0900

    chore: remove unnecessary return

--- Sources/Firework/APIRequest.swift
+++ Sources/Firework/APIRequest.swift
@@ -42,7 +42,7 @@ public protocol Postable {
 public protocol GETRequest: APIRequest {}
 
 public extension GETRequest {
-    static var httpMethod: HTTPMethod { return .get }
+    static var httpMethod: HTTPMethod { .get }
 }
 
 // MARK: - POSTRequest
@@ -50,7 +50,7 @@ public extension GETRequest {
 public protocol POSTRequest: APIRequest, Postable {}
 
 public extension POSTRequest {
-    static var httpMethod: HTTPMethod { return .post }
+    static var httpMethod: HTTPMethod { .post }
 }
 
 // MARK: - PUTRequest
@@ -58,7 +58,7 @@ public extension POSTRequest {
 public protocol PUTRequest: APIRequest, Postable {}
 
 public extension PUTRequest {
-    static var httpMethod: HTTPMethod { return .put }
+    static var httpMethod: HTTPMethod { .put }
 }
 
 // MARK: - PUTRequest
@@ -66,7 +66,7 @@ public extension PUTRequest {
 public protocol DELETERequest: APIRequest {}
 
 public extension DELETERequest {
-    static var httpMethod: HTTPMethod { return .delete }
+    static var httpMethod: HTTPMethod { .delete }
 }
 
 // MARK: - DecodingRequest
