e346513c35b692804c7b84a4e226cc305a87b493
Yusaku Nishi <jrsaruo@gmail.com>
2022-12-07 22:45:36 +0900

    add: AlamofireAdaptor.session

--- Sources/Firework/HTTPClient/AFClient.swift
+++ Sources/Firework/HTTPClient/AFClient.swift
@@ -10,7 +10,13 @@ import Foundation
 
 // MARK: - AlamofireAdaptor -
 
-public struct AlamofireAdaptor: HTTPClientAdaptor, Sendable {
+public struct AlamofireAdaptor: HTTPClientAdaptor {
+    
+    private let session: Session
+    
+    init(configuration: HTTPClientConfiguration) {
+        session = Session(configuration: configuration.urlSession)
+    }
     
     public func send(_ request: some HTTPRequest,
                      receiveOn queue: DispatchQueue = .main,
@@ -29,18 +35,22 @@ public struct AlamofireAdaptor: HTTPClientAdaptor, Sendable {
     }
     
     private func makeDataRequest<Request: HTTPRequest>(from request: Request) -> DataRequest {
-        AF.request(request.urlComponents,
-                   method: Request.httpMethod,
-                   parameters: (request as? any HTTPBodySendable)?.body,
-                   encoding: JSONEncoding.default,
-                   headers: request.headers)
+        session
+            .request(request.urlComponents,
+                     method: Request.httpMethod,
+                     parameters: (request as? any HTTPBodySendable)?.body,
+                     encoding: JSONEncoding.default,
+                     headers: request.headers)
             .validate(statusCode: request.acceptableStatusCodes)
             .validate(contentType: request.acceptableContentTypes)
     }
 }
 
 public extension HTTPClientAdaptor where Self == AlamofireAdaptor {
-    static var alamofire: Self { AlamofireAdaptor() }
+    
+    static func alamofire(configuration: HTTPClientConfiguration) -> Self {
+        AlamofireAdaptor(configuration: configuration)
+    }
 }
 
 // MARK: - AFClient -
@@ -53,6 +63,7 @@ extension AFClient {
     /// Creates an ``AFClient`` instance.
     /// - Parameter configuration: A configuration used for HTTP communication. The default is `.shared`.
     public init(configuration: HTTPClientConfiguration = .shared) {
-        self.init(configuration: configuration, adaptor: .alamofire)
+        self.init(configuration: configuration,
+                  adaptor: .alamofire(configuration: configuration))
     }
 }
